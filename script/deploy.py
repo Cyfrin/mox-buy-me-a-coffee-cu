919adae0a6d2ae240127ee0e040dea0bc87eeb41
Patrick Collins <54278053+PatrickAlphaC@users.noreply.github.com>
2024-11-16 16:47:55 -0500

    added withdraw

--- script/deploy.py
+++ script/deploy.py
@@ -7,16 +7,15 @@ def deploy_coffee(price_feed: VyperContract) -> VyperContract:
     print("Using price feed:", price_feed.address)
     coffee: VyperContract = buy_me_a_coffee.deploy(price_feed.address)
     print("Deployed coffee contract at:", coffee.address)
-
-    active_network = get_active_network()
-    if active_network.has_explorer():
-        print("Verifying contract on explorer...")
-        result = active_network.moccasin_verify(coffee)
-        result.wait_for_verification()
     return coffee
 
 
 def moccasin_main() -> VyperContract:
     active_network = get_active_network()
     price_feed = active_network.manifest_named("price_feed")
-    return deploy_coffee(price_feed)
+    coffee = deploy_coffee(price_feed)
+    if active_network.has_explorer() and active_network.is_local_or_forked_network() is False:
+        print("Verifying contract on explorer...")
+        result = active_network.moccasin_verify(coffee)
+        result.wait_for_verification()
+    return coffee
